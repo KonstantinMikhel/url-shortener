e1521ec6a5a4909fe8d68f66444e305197102710
Konstantin Mikhel <mikhelkonstantin@gmail.com>
2024-02-12 18:38:29 +0300

    Refactor handlers

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,97 +1,20 @@
 package main
 
 import (
-	"io"
-	"math"
 	"net/http"
-	"slices"
-	"strings"
+
+	"github.com/KonstantinMikhel/url-shortener/internal/handlers"
 )
 
-type ShortenedURL struct {
-	LongURL  string
-	ShortURL string
-}
-
-var urls []ShortenedURL = make([]ShortenedURL, 0)
-
-const base62Digits = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-func DecimalToBase62(n int64) string {
-	if n == 0 {
-		return "0"
-	}
-
-	base62 := make([]byte, 0)
-	radix := int64(62)
-
-	for n > 0 {
-		remainder := n % radix
-		base62 = append([]byte{base62Digits[remainder]}, base62...)
-		n /= radix
-	}
-
-	return string(base62)
-}
-
-func Base62ToDecimal(s string) int64 {
-	var decimalNumber int64
-
-	for i, c := range s {
-		decimalNumber += int64(strings.IndexByte(base62Digits, byte(c))) * int64(math.Pow(62, float64(len(s)-i-1)))
-	}
-
-	return decimalNumber
-}
-
-func handleURL(wr http.ResponseWriter, req *http.Request) {
-	switch req.Method {
+func handleURL(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
 	case http.MethodGet:
-		handleOriginalURL(wr, req)
+		handlers.HandleRedirect(writer, request)
 	case http.MethodPost:
-		handleShortenedURL(wr, req)
+		handlers.HandleShorten(writer, request)
 	default:
-		http.Error(wr, "Only GET and POST requests are allowed!", http.StatusBadRequest)
+		http.Error(writer, "Only GET and POST requests are allowed!", http.StatusBadRequest)
 	}
-}
-
-func handleShortenedURL(wr http.ResponseWriter, req *http.Request) {
-	wr.Header().Set("Content-Type", "text/plain")
-	wr.WriteHeader(http.StatusCreated)
-
-	body, _ := io.ReadAll(req.Body)
-	longURL := string(body)
-
-	idx := slices.IndexFunc(urls, func(url ShortenedURL) bool {
-		return url.LongURL == longURL
-	})
-
-	if idx == -1 {
-		urls = append(urls, ShortenedURL{
-			LongURL:  longURL,
-			ShortURL: DecimalToBase62(int64(len(urls)) + 100000),
-		})
-		wr.Write([]byte(`http://localhost:8080/` + urls[len(urls)-1].ShortURL))
-	} else {
-		wr.Write([]byte(`http://localhost:8080/` + urls[idx].ShortURL))
-	}
-}
-
-// GET
-func handleOriginalURL(wr http.ResponseWriter, req *http.Request) {
-	urlParts := strings.Split(req.URL.Path, "/")
-	shortURL := urlParts[len(urlParts)-1]
-
-	idx := slices.IndexFunc(urls, func(url ShortenedURL) bool {
-		return url.ShortURL == shortURL
-	})
-
-	if idx == -1 {
-		http.Error(wr, "Only GET and POST requests are allowed!", http.StatusBadRequest)
-	}
-
-	wr.Header().Set("Location", urls[idx].LongURL)
-	wr.WriteHeader(http.StatusTemporaryRedirect)
 }
 
 func main() {
---
+++ internal/handlers/handlers.go
@@ -0,0 +1,68 @@
+package handlers
+
+import (
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"strings"
+)
+
+var urls = make(map[string]string)
+
+func HandleShorten(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodPost {
+		http.Error(writer, "Invalid request method", http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(request.Body)
+	if err != nil {
+		http.Error(writer, "Could not read request body", http.StatusBadRequest)
+		return
+	}
+
+	originalURL := string(body)
+	if originalURL == "" {
+		http.Error(writer, "Original URL is missing in a request body", http.StatusBadRequest)
+		return
+	}
+
+	shortURL := generateShortURL()
+	urls[shortURL] = originalURL
+
+	writer.Header().Set("Content-Type", "text/plain")
+	writer.WriteHeader(http.StatusCreated)
+
+	fullShortenedURL := fmt.Sprintf("http://localhost:8080/%s", shortURL)
+	writer.Write([]byte(fullShortenedURL))
+}
+
+func HandleRedirect(writer http.ResponseWriter, request *http.Request) {
+	shortURL := strings.TrimPrefix(request.URL.Path, "/")
+	if shortURL == "" {
+		http.Error(writer, "Shortened URL is missing in a request", http.StatusBadRequest)
+		return
+	}
+
+	// Retrieve the original URL
+	originalURL, found := urls[shortURL]
+	if !found {
+		http.Error(writer, "Shortened URL not found", http.StatusBadRequest)
+		return
+	}
+
+	// Redirect the user to the original URL
+	http.Redirect(writer, request, originalURL, http.StatusMovedPermanently)
+}
+
+func generateShortURL() string {
+	const alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	const shortURLLength = 8
+
+	shortURL := make([]byte, shortURLLength)
+	for i := range shortURL {
+		shortURL[i] = alphabet[rand.Intn(len(alphabet))]
+	}
+	return string(shortURL)
+}
